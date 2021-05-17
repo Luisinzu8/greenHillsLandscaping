b98a4d1a12eb7aba5f9d2efbe3902013eca94b05
Francisco Martinez <panchetz7@gmail.com>
2021-05-17 13:50:23 -0700

    create webpack bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,42 @@
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  context: __dirname + '/',
+  entry: path.resolve(__dirname, './src/index.js'),
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+  },
+  devtool: 'eval',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      },
+      { test: /\.html$/, loader: 'html-loader' },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + '/Template HTML/index.html',
+      filename: 'index.html',
+      inject: 'body',
+    }),
+  ],
+};
 
 // module.exports = {
 //   context: __dirname + '/',
@@ -28,40 +65,4 @@
 //       },
 //     ],
 //   },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       template: __dirname + '/src/index.html',
-//       filename: 'index.html',
-//       inject: 'body',
-//     }),
-//   ],
 // };
-
-module.exports = {
-  context: __dirname + '/',
-  entry: path.resolve(__dirname, './src/index.js'),
-  output: {
-    path: __dirname + '/dist',
-    filename: 'bundle.js',
-  },
-  devtool: 'eval',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
-      {
-        test: /\.css$/i,
-        exclude: /node_modules/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-};
