bbef389f8686c66ef8e8fd4e9d697f0ffb913695
Francisco Martinez <panchetz7@gmail.com>
2021-05-19 00:35:05 -0700

    played with plugin

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
   },
   // plugins: [
   //   new HtmlWebpackPlugin({
-  //     template: __dirname + '/public/index.html',
+  //     template: __dirname + '/Template HTML/index.html',
   //     filename: 'index.html',
   //     inject: 'body',
   //   }),
