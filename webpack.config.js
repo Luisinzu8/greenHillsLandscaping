b640b0c1a2101bed8001d1b1467cecdaf2f0ba05
Francisco Martinez <panchetz7@gmail.com>
2021-05-19 00:03:30 -0700

    push for review

--- webpack.config.js
+++ webpack.config.js
@@ -29,40 +29,11 @@ module.exports = {
       { test: /\.html$/, loader: 'html-loader' },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: __dirname + '/Template HTML/index.html',
-      filename: 'index.html',
-      inject: 'body',
-    }),
-  ],
+  // plugins: [
+  //   new HtmlWebpackPlugin({
+  //     template: __dirname + '/public/index.html',
+  //     filename: 'index.html',
+  //     inject: 'body',
+  //   }),
+  // ],
 };
-
-// module.exports = {
-//   context: __dirname + '/',
-//   entry: path.resolve(__dirname, './src/index.js'),
-//   output: {
-//     path: __dirname + '/dist',
-//     filename: 'bundle.js',
-//   },
-//   devtool: 'eval',
-//   module: {
-//     rules: [
-//       {
-//         test: /\.(js|jsx)$/,
-//         exclude: /node_modules/,
-//         use: {
-//           loader: 'babel-loader',
-//           options: {
-//             presets: ['@babel/preset-env'],
-//           },
-//         },
-//       },
-//       {
-//         test: /\.css$/i,
-//         exclude: /node_modules/,
-//         use: ['style-loader', 'css-loader'],
-//       },
-//     ],
-//   },
-// };
