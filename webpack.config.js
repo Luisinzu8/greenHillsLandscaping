9b0179ce36d732cdce63ce60ad94146f36fab098
Francisco Martinez <panchetz7@gmail.com>
2021-06-16 14:41:37 -0700

    htmlWebpackPlugin successful

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,39 @@
-// // const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
-// module.exports = {
-//   context: __dirname + '/',
-//   entry: path.resolve(__dirname, './src/index.js'),
-//   output: {
-//     path: __dirname + '/dist',
-//     filename: 'bundle.js',
-//   },
-//   devtool: 'eval',
-//   module: {
-//     rules: [
-//       {
-//         test: /\.(js|jsx)$/,
-//         exclude: /node_modules/,
-//         use: {
-//           loader: 'babel-loader',
-//           options: {
-//             presets: ['@babel/preset-env'],
-//           },
-//         },
-//       },
-//       {
-//         test: /\.css$/i,
-//         exclude: /node_modules/,
-//         use: ['style-loader', 'css-loader'],
-//       },
-//       { test: /\.html$/, loader: 'html-loader' },
-//     ],
-//   },
-//   // plugins: [
-//   //   new HtmlWebpackPlugin({
-//   //     template: __dirname + '/Template HTML/index.html',
-//   //     filename: 'index.html',
-//   //     inject: 'body',
-//   //   }),
-//   // ],
-// };
+module.exports = {
+  context: __dirname + '/',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devtool: 'eval',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      },
+      { test: /\.html$/, loader: 'html-loader' },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      // template: 'Static Application/index.html',
+      filename: './dist/index.html',
+      inject: 'body',
+    }),
+  ],
+};
